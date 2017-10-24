65760e6a43f9fc05b2e77338a27597f21c88f272
1ike <googlohren@gmail.com>
2017-10-24 15:28:01 +0300

    add linter and travis

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# project-lvl1-s156
+[![Maintainability](https://api.codeclimate.com/v1/badges/27fe7120df4b722af561/maintainability)](https://codeclimate.com/github/1ike/project-lvl1-s156/maintainability)
--- src/Cli.php
+++ src/Cli.php
@@ -1,10 +1,12 @@
 <?php
+
 namespace BrainGames\Cli;
 
 use function \cli\line;
 
-function run() {
-  line('Welcome to the Brain Game!' . PHP_EOL);
-  $name = \cli\prompt('May I have your name?');
-  line("Hello, %s!", $name);
+function run()
+{
+    line('Welcome to the Brain Game!'.PHP_EOL);
+    $name = \cli\prompt('May I have your name?');
+    line("Hello, %s!", $name);
 }

8fb1cf37eee65ef43f7f8ea9bbcc438618fed7b7
1ike <googlohren@gmail.com>
2017-10-24 13:18:42 +0300

    add greeting by name

---
+++ src/Cli.php
@@ -0,0 +1,10 @@
+<?php
+namespace BrainGames\Cli;
+
+use function \cli\line;
+
+function run() {
+  line('Welcome to the Brain Game!' . PHP_EOL);
+  $name = \cli\prompt('May I have your name?');
+  line("Hello, %s!", $name);
+}

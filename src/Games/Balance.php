8de758dbdd6e856ce83a7a597900db4171d7a5f9
1ike <googlohren@gmail.com>
2017-10-25 18:06:38 +0300

    Add brain-balance game.

---
+++ src/Games/Balance.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace BrainGames\Games;
+
+class Balance extends \BrainGames\Game
+{
+
+    private function balance($num)
+    {
+        $str = (string) $num;
+
+        $input = str_split($str);
+        $inputLength = count($input);
+
+        $inputDigit = array_map(function ($val) {
+            return (int) $val;
+        }, $input);
+
+        $sum = array_reduce($inputDigit, function ($acc, $val) {
+            return $acc + $val;
+        });
+
+        $evenDigit = round($sum / $inputLength);
+        $remainder = $sum % $inputLength;
+
+        $output = [];
+        while ($remainder > 0) {
+            array_push($output, $evenDigit + 1);
+            $remainder -= 1;
+        }
+        while ($inputLength > count($output)) {
+            array_push($output, $evenDigit);
+        }
+
+        return (int) implode(array_reverse($output));
+    }
+
+
+    public static function run()
+    {
+        $rulesMessage = 'Find the greatest common divisor of given numbers.';
+        $limitMaxNumber = 1000;
+
+        $getQuestionAnswerPair = function () use ($rulesMessage, $limitMaxNumber) {
+
+            $num = rand(1, $limitMaxNumber);
+            $question = $num;
+
+            $correctAnswer = (string) self::balance($num);
+
+            return [
+                'question' => $question,
+                'correctAnswer' => $correctAnswer
+            ];
+        };
+
+        $game = new self($rulesMessage, $getQuestionAnswerPair);
+        $game->start();
+    }
+}

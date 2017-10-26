4a732089d9e6bc8dd9f157b0d5bd325ade0129d0
1ike <googlohren@gmail.com>
2017-10-26 12:41:00 +0300

    Add Optimus Prime game.

---
+++ src/Games/Prime.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace BrainGames\Games;
+
+class Prime extends \BrainGames\Game
+{
+
+    private function isPrime($num, $upperLimit, $next = 2) {
+
+        if ($next > $upperLimit) return true;
+
+        if ($num % $next == 0 || num <= 1) {
+            return false;
+        }
+
+        return self::isPrime($num, $upperLimit, $next + 1);
+    }
+
+
+    public static function run()
+    {
+        $rulesMessage = 'Balance the given number.';
+
+
+        $getQuestionAnswerPair = function () {
+
+            $limitMinNumber = 1;
+            $limitMaxNumber = 50;
+
+            $question = rand($limitMinNumber, $limitMaxNumber);
+
+            $isPrime = self::isPrime($question, sqrt($question));
+            $correctAnswer = $isPrime ? 'yes' : 'no';
+
+            return [
+                'question' => $question,
+                'correctAnswer' => $correctAnswer
+            ];
+        };
+
+        $game = new self($rulesMessage, $getQuestionAnswerPair);
+        $game->start();
+    }
+}
